ed264f0b403eaf1a1701289cd15d2497630f673c
Aleksandr <majoryman90@gmail.com>
2023-04-04 19:38:59 +0300

    homework4

--- script.js
+++ script.js
@@ -1,69 +1,55 @@
 'use strict';
 
-// Задача 1
-function numberCube(number) {
-    return (number ** 3);
-}
-let sum = (numberCube(2) + numberCube(3));
+// Задание 1
+// Необходимо с помощью цикла for вывести следующие 11 строк в консоль:
+// 0 – это ноль
+// 1 – нечетное число
+// 2 – четное число
+// 3 – нечетное число
+// …
+// 10 – четное число
+// for (let i = 0; i < 11; i++) {
+//     if (i === 0) {
+//         console.log(`${i} - это ноль`);
+//     } else if (i % 2 == 0) {
+//         console.log(`${i} - четное число`);
+//     } else if (i % 2 != 0) {
+//         console.log(`${i} - нечетное число`);
+//     }
+// }
 
+// Задание 2
+// Дан массив [1, 2, 3, 4, 5, 6, 7]
+// Сделайте из этого массива следующий [1, 2, 3, 6, 7]
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.splice(3, 2);
+// console.log(arr);
+
+// Задание 3
+// Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 таких элементов
+// 1. Рассчитать сумму элементов этого массива
+// 2. Найти минимальное число
+// 3. Найти есть ли в этом массиве число 3
+
+function generateArray(length) {
+    const array = [];
+    for (let i = 0; i < length; i++) {
+        array.push(Math.floor(Math.random() * 9));
+    }
+    return array;
+
+}
+
+const newArray = generateArray(5);
+console.log(newArray);
+let sum = 0;
+for (let i = 0; i < newArray.length; i++) {
+    sum += newArray[i];
+}
 console.log(sum);
 
-
-//Задача 2
-function minusPercent() {
-    let sallary = prompt('Введите размер вашей зарплаты цифрой', 3000);
-    if (isNaN(sallary) || sallary == undefined || sallary == '') {
-        alert('Нужно написать число!');
-    } else {
-        return (sallary * 0.77);
+for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] === 3) {
+        console.log(`В массиве есть число 3`);
     }
 }
-
-// console.log(minusPercent());
-
-
-//Задача 3
-function maxNumber() {
-    let num1 = Number(prompt('Введите первое число'));
-    let num2 = Number(prompt('Введите второе число'));
-    let num3 = Number(prompt('Введите третье число'));
-    if (num1 > num2 && num1 > num3) {
-        console.log(`Число ${num1} максимальное`);
-    } else if (num2 > num1 && num2 > num3) {
-        console.log(`Число ${num2} максимальное`);
-    } else if (num3 > num1 && num3 > num2) {
-        console.log(`Число ${num3} максимальное`);
-    } else {
-        console.log(`Вы ввели НЕ число`);
-    }
-}
-
-maxNumber();
-
-
-//Задача 4
-function summa(num1, num2) {
-    return (num1 + num2);
-}
-
-function difference(num1, num2) {
-    if (num1 > num2) {
-        return (num1 - num2);
-    } else {
-        return (num2 - num1);
-    }
-}
-
-function multiplication(num1, num2) {
-    return (num1 * num2);
-}
-
-function division(num1, num2) {
-    return (num1 / num2);
-}
-
-console.log(summa(2, 6));
-console.log(difference(2, 6));
-console.log(difference(6, 6));
-console.log(multiplication(2, 6));
-console.log(division(2, 6));
